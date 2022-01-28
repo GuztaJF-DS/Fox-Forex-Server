7d1dc2de9cca12db0d52ca7ce3bd3cfddd9bb2d6
GuztaJF-DS <noobshippuden13>
2022-01-28 08:47:08 -0300

    final commit

--- src/controllers/forex.ts
+++ src/controllers/forex.ts
@@ -2,16 +2,6 @@ import express,{Request,Response} from 'express';
 const router=express();
 const axios=require("axios");
 
-type requestBodyReturnProfit={
-    Opening:number,//for the Opening you catch the latest Trade's collection Mid value and use it 
-    Closure:number,//for the Closure you catch the Actual Mid value and use it
-    IsABuy:boolean,//this says if the user is Buying or selling(True=Buy & False=Sell)
-    Lots:number,//Number of lots Buyed
-    SwapTax:number,//Tax
-    TotalDaysPassed:number//The Amount of nights that a trade Stayed Open 
-}
-
-
 router.post('/getdailyhistory',async(req:Request,res:Response)=>{
     try{
         let AxiosRes=await axios({
